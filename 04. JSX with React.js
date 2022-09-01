d0a23a41eaae4c3392da0c7f6f27e71f6ea1925c
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-09-01 22:30:38 +0530

    Create 04. JSX with React.js

---
+++ 04. JSX with React.js
@@ -0,0 +1,19 @@
+// JSX with React 
+// - JSX : JavaScript XML
+// - JSX is simply using HTML with JavaScript 
+// - We don't need to install anything from outside for using JSX
+// - We can also use React without using JSX but it becomes more complicate 
+// Interview Question :-
+// Q. Can we use React and Angular in same project?
+// Ans : Yes, As React is library it can use in any project
+
+function User() {
+    return (
+        <div>
+            <div>Hello</div>
+            <div>{10 + 20}</div>
+        </div>
+    )
+}
+
+export default User;
