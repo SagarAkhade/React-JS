50195df01037437fe0c1059d41357b7f2a03f1ed
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-09-01 21:55:05 +0530

    Create 03. class component.js

---
+++ 03. class component.js
@@ -0,0 +1,27 @@
+// Class Component ->>
+//- Class components are used very less
+// - For using class Component first import react 
+// Syntax : import React,{Component} from 'react'
+//- All other things are similar as function components
+//- We can consume this class component in other file as HTML tag like <NameOfFile />
+/*
+Syntax :
+class NameOfFile extends Component{
+    render(){
+        return (
+            <HTML codes> 
+        ) 
+    } 
+} 
+*/
+
+import React,{Component} from "react";
+class User extends Component {
+    render(){
+        return (
+            <h1> Hello from User </h1>
+        )
+    }
+}
+
+export default User;
