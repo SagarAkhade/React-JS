1e793cc670ce6a461d22fb9c6c9f9aff18cf7ada
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-09-06 17:35:50 +0530

    Create 45. Postman.js

---
+++ 45. Postman.js
@@ -0,0 +1,24 @@
+// Postman ->>
+// - We are using Postman here for testing APIs 
+// - Postman use in frontend as well as backend
+// - To install API go to google and type "postman download" and download postman from there
+// - We can directly use postman without installing 
+// - Just go to google chrome and type -> "postman chrome plugin"
+// - And then postman is install in google chrome browser
+// - To use this intalled postman go to new page->App section-> click Postman app
+// - Another way to Access postman Chrome settings->Extension->Postman
+// GET Method ->
+// - Now open Postmant and in that paste our API link there 
+// - Check that we have selected GET method for testing API, 
+// if it prints the data of API then it means API working successfully
+// POST Method ->
+// - To send data through the postman  
+// select POST method->select Body->select raw->click on JSON
+// - And then add raw JSON data there then click on send 
+// PUT Method->
+// - To update current data we use PUT Method
+// - Select PUT method->select Body->select raw->click on JSON
+// - and then paste complete JSON data and then update it whatever want to update 
+// DELETE Method ->
+// - To delete any user data we use DELETE Method
+// - Select DELETE Method -> paste link in input block->Delete the last userNumber->click on send
