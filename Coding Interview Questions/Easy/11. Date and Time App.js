8abad3a44b3d6fd1e8aae197747b3d75e796ee2b
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-09-13 22:22:46 +0530

    Create 11. Date and Time App.js

---
+++ Coding Interview Questions/Easy/11. Date and Time App.js
@@ -0,0 +1,56 @@
+/* 
+Q.  There is a three count state count A, count B and count C 
+   with initial state 0, and a button (Click Me). so every 5th value of
+   count A, count B should be increase by one and every 10th value of 
+   count A, count C should be increase by one
+*/
+import logo from './logo.svg';
+
+import './App.css';
+import { Table } from 'react-bootstrap'
+import { Component, useRef, useState, useEffect } from 'react';
+
+import DateAndTimeApp from './Components/DateAndTimeApp/DateAndTimeApp';
+/* This is parent Component */
+
+function App() {
+  return (
+    <div className='App'>
+      <DateAndTimeApp />
+    </div>
+  )
+}
+
+export default App;
+
+/*---------------------------*/
+/* This is child component */
+
+import { useState } from "react";
+
+function DateAndTimeApp() {
+    const [render, setRender] = useState(false);
+    function display() {
+        setRender(!render);
+    }
+
+    return (
+        <>
+            <DisplayDateTime />
+            <button onClick={display}>Show current Date and Time</button>
+        </>
+
+    )
+}
+
+function DisplayDateTime() {
+    const timmer = new Date();
+
+    return (
+        <>
+            <h1>Date : {timmer.getDate()}/{timmer.getMonth() + 1}/{timmer.getFullYear()}</h1>
+            <h1>Current Time : {timmer.toLocaleTimeString()}</h1>
+        </>
+    )
+}
+export default DateAndTimeApp;
